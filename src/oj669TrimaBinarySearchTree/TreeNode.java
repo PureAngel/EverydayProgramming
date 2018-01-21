60d1cdd576530dda640edcd94d78e63d5a55dba6
PureAngel <pureangelbxy@163.com>
2018-01-20 17:11:01 -0800

    669

---
+++ src/oj669TrimaBinarySearchTree/TreeNode.java
@@ -0,0 +1,11 @@
+package oj669TrimaBinarySearchTree;
+
+/**
+ * Created by xueyingbai on 1/20/18.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
