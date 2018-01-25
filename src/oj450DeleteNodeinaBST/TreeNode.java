7004bdb06bd84d5c5d4ad9115537f12de580ec63
PureAngel <pureangelbxy@163.com>
2018-01-24 17:39:26 -0800

    450

---
+++ src/oj450DeleteNodeinaBST/TreeNode.java
@@ -0,0 +1,11 @@
+package oj450DeleteNodeinaBST;
+
+/**
+ * Created by xueyingbai on 1/24/18.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
