fb0a934dbbeaca1a5f6ce7b4c6b9d76ea82e67c9
PureAngel <pureangelbxy@163.com>
2018-02-03 17:45:00 -0800

    “671”

---
+++ src/oj671SecondMinimumNodeInaBinaryTree/TreeNode.java
@@ -0,0 +1,11 @@
+package oj671SecondMinimumNodeInaBinaryTree;
+
+/**
+ * Created by xueyingbai on 2/3/18.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
