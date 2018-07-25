4930188928f73fba21a8929eb688c9d465315b5d
PureAngel <pureangelbxy@163.com>
2018-07-25 23:32:21 +0800

    084

---
+++ src/oj084LargestRectangleInHistogram/LargestRectangleInHistogram.java
@@ -0,0 +1,17 @@
+package oj084LargestRectangleInHistogram;
+
+/**
+ * Given n non-negative integers representing the histogram's bar height where the width of each bar is 1,
+ * find the area of largest rectangle in the histogram.
+ *
+ * Example:
+ *
+ * Input: [2,1,5,6,2,3]
+ * Output: 10
+ */
+
+public class LargestRectangleInHistogram {
+    public int largestRectangleArea(int[] heights) {
+
+    }
+}
