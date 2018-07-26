af3d9b99b3eca131348a02ed439ab3d40f0340ec
baixueying <xueyingbai@newbanker.cn>
2018-07-26 13:01:35 +0800

    084

--- src/oj084LargestRectangleInHistogram/LargestRectangleInHistogram.java
+++ src/oj084LargestRectangleInHistogram/LargestRectangleInHistogram.java
@@ -12,6 +12,19 @@ package oj084LargestRectangleInHistogram;
 
 public class LargestRectangleInHistogram {
     public int largestRectangleArea(int[] heights) {
+        return getArea(heights, 0, heights.length - 1);
+    }
 
+    private int getArea(int[] heights, int start, int end) {
+        if(start > end) {
+            return 0;
+        }
+        int minIndex = start;
+        for(int i = start; i <= end; i++) {
+            if(heights[i] < heights[minIndex]) {
+                minIndex = i;
+            }
+        }
+        return Math.max(heights[minIndex] * (end - start + 1), Math.max(getArea(heights, start, minIndex - 1), getArea(heights, minIndex + 1, end)));
     }
 }
