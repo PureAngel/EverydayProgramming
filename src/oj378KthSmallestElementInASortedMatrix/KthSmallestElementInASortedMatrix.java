ce7bde516da4552afa97c647c6aeb8829c22c441
PureAngel <pureangelbxy@163.com>
2018-07-28 09:01:15 +0800

    378

--- src/oj378KthSmallestElementInASortedMatrix/KthSmallestElementInASortedMatrix.java
+++ src/oj378KthSmallestElementInASortedMatrix/KthSmallestElementInASortedMatrix.java
@@ -21,6 +21,23 @@ package oj378KthSmallestElementInASortedMatrix;
 
 public class KthSmallestElementInASortedMatrix {
     public int kthSmallest(int[][] matrix, int k) {
-
+        int low = matrix[0][0], high = matrix[matrix.length - 1][matrix[0].length - 1] + 1; // [low, high)
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
+            int j = matrix[0].length - 1;
+            for(int i = 0; i < matrix.length; i++) {
+                while (j >= 0 && matrix[i][j] > mid) {
+                    j--;
+                }
+                count += (j + 1);
+            }
+            if(count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
     }
 }
