89d9e817ab8368722b6d47b33d45d12df4d79c84
PureAngel <pureangelbxy@163.com>
2018-08-03 14:58:28 +0800

    086_2nd

---
+++ src/oj086_2nd/ListNode.java
@@ -0,0 +1,7 @@
+package oj086_2nd;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ src/oj086_2nd/PartitionList.java
@@ -0,0 +1,34 @@
+package oj086_2nd;
+
+/**
+ * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+ *
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+ *
+ * Example:
+ *
+ * Input: head = 1->4->3->2->5->2, x = 3
+ * Output: 1->2->2->4->3->5
+ */
+
+public class PartitionList {
+    public ListNode partition(ListNode head, int x) {
+        ListNode less = new ListNode(0);
+        ListNode more = new ListNode(0);
+        ListNode lessNode = less;
+        ListNode moreNode = more;
+        while(head != null) {
+            if(head.val < x) {
+                lessNode.next = head;
+                lessNode = lessNode.next;
+            } else {
+                moreNode.next = head;
+                moreNode = moreNode.next;
+            }
+            head = head.next;
+        }
+        lessNode.next = more.next;
+        moreNode.next = null;
+        return less.next;
+    }
+}
